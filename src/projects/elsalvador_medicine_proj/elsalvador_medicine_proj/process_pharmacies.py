a1f1b16c83664cbb2c1e83bc2780579bddb5b0cc
Javigoncompte <javiergonzalez7@gmail.com>
2023-12-24 17:00:49 -0600

    cassandra

--- src/projects/elsalvador_medicine_proj/elsalvador_medicine_proj/process_pharmacies.py
+++ src/projects/elsalvador_medicine_proj/elsalvador_medicine_proj/process_pharmacies.py
@@ -1,3 +1,5 @@
+import polars as pl
+from cassandra.cluster import Cluster
 from client.pharmacies.farmavalue import FarmaValue
 from client.pharmacies.onlife import Onlife
 from client.pharmacies.san_nicolas import SanNicolas
@@ -12,7 +14,10 @@ def get_data(data: list[dict]) -> list[dict]:
         onlife_data = onlife.process(commercial_name)
         san_nicolas_data = san_nicolas.process(commercial_name)
         farma_value_data = farma_value.process(commercial_name)
-    return
+        med.update(onlife_data).update(san_nicolas_data).update(farma_value_data)
+    return data
+
 
 def format_data(data: list[dict]) -> list[dict]:
-    pass
+    df = pl.DataFrame(data)
+    return df
